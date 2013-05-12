4e71bcc76a1516fa78cc507ed12c24549a974ac5
Markus Thielen <info@logicx.de>
2013-05-12 20:38:23 +0200

    containerawarewildcardeventdispatcher

---
+++ src/Infrastructure/Eventing/EventLogger.php
@@ -0,0 +1,26 @@
+<?php
+namespace Infrastructure\Eventing;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\Event;
+use Monolog\Logger;
+
+class EventLogger
+{
+
+	/**
+	 * @var Logger
+	 */
+	private $logger;
+
+	public function __construct(Logger $logger)
+	{
+		$this->logger = $logger;
+	}
+
+	public function logEvent(Event $event)
+	{
+		$this->logger->addDebug('Event fired: '.$event->getName() . ', class: '.get_class($event));
+	}
+
+}
