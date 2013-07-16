1df5452b4e652b2f9ba9a8e332fd2ad8ba57261c
Markus <Markus@Markus-PC>
2013-07-16 14:19:04 +0200

    validation utils

--- src/Infrastructure/Validation/Utils.php
+++ src/Infrastructure/Validation/Utils.php
@@ -6,7 +6,7 @@ class Utils
 
     public static function isMysqlDate($subject)
     {
-        return preg_match('^([1-9]|[12][0-9]|3[01])[- /.]([1-9]|1[012])[- /.](19|20)\d\d$', $subject);
+        return preg_match('/^([1-9]|[12][0-9]|3[01])[- /.]([1-9]|1[012])[- /.](19|20)\d\d$/', $subject);
     }
 
 }
