7def353b25425cc5eacf0dede47dacf6e2e733a6
Markus <Markus@Markus-PC>
2013-07-18 10:41:42 +0200

    validation

--- src/Infrastructure/Validation/Utils.php
+++ src/Infrastructure/Validation/Utils.php
@@ -4,9 +4,26 @@ namespace Infrastructure\Validation;
 class Utils
 {
 
+    /**
+     * returns true if valid mysql date (Y-m-d)
+     *
+     * @param unknown $subject
+     * @return number
+     */
     public static function isMysqlDate($subject)
     {
-        return preg_match('/^([1-9]|[12][0-9]|3[01])[- /.]([1-9]|1[012])[- /.](19|20)\d\d$/', $subject);
+        return preg_match('/^\d{4}-\d{2}-\d{2}$/', $subject);
+    }
+
+    /**
+     * returns true if integer (as string or as real int type)
+     *
+     * @param unknown $input
+     * @return boolean
+     */
+    public static function isInteger($input)
+    {
+        return(ctype_digit(strval($input)));
     }
 
 }
---
+++ tests/Unit/Validation/UtilsTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace Infrastructure\Validation;
+
+class UtilsTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider getIsIntegerData
+     */
+	public function testIsInteger($input, $expectedResult)
+	{
+        $actualResult = Utils::isInteger($input);
+        $this->assertEquals($expectedResult, $actualResult);
+	}
+
+	public function getIsIntegerData()
+	{
+	    return array(
+            array(23, true),
+            array("23", true),
+            array(23.5, false),
+            array("23.5", false),
+            array(null, false),
+            array("", false)
+	    );
+	}
+
+	/**
+	 * @dataProvider getIsMysqlDateData
+	 */
+	public function testIsMysqlDate($input, $expectedResult)
+	{
+	    $actualResult = Utils::isMysqlDate($input);
+	    $this->assertEquals($expectedResult, $actualResult);
+	}
+
+	public function getIsMysqlDateData()
+	{
+	    return array(
+            array("2013-01-01", true),
+            array("9999-50-50", true),
+            array("2013/01/01", false),
+            array("5", false),
+            array(null, false),
+            array("", false)
+	    );
+	}
+
+}
