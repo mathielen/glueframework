e68420da805892006f103282f0e53b84e2c9b14c
Markus <Markus@Markus-PC>
2013-08-14 20:17:49 +0200

    minor fix

--- src/Infrastructure/Exception/ResourceNotFoundException.php
+++ src/Infrastructure/Exception/ResourceNotFoundException.php
@@ -6,7 +6,7 @@ class ResourceNotFoundException extends \Exception
 
     public function __construct($resourceName, $identifier)
     {
-        parent::__construct(sprintf('Resource "%s" with id %s not found', $resourceName, $identifier));
+        parent::__construct(sprintf("Resource '%s' with id '%s' not found", $resourceName, $identifier));
     }
 
 }
