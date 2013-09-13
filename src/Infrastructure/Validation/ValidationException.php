4e885c153a6388c73a20bb96e2835da7b6e91e22
Markus <Markus@Markus-PC>
2013-09-13 17:19:09 +0200

    validationexception

--- src/Infrastructure/Validation/ValidationException.php
+++ src/Infrastructure/Validation/ValidationException.php
@@ -1,5 +1,18 @@
 <?php
 namespace Infrastructure\Validation;
 
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 class ValidationException extends \Exception
-{}
+{
+
+    public static function fromConstraintViolations(ConstraintViolationListInterface $validationErrors)
+    {
+        $messages = array();
+        foreach ($validationErrors as $error) {
+            $messages[] = $error->getMessage();
+        }
+
+        return new self(join("\n", $messages));
+    }
+
+}
