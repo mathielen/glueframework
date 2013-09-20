9fe12abf736f9350d2a2ea81410a8843cfacc4f1
Markus <Markus@Markus-PC>
2013-09-20 09:59:52 +0200

    validation exception

--- src/Infrastructure/Validation/ValidationException.php
+++ src/Infrastructure/Validation/ValidationException.php
@@ -5,14 +5,17 @@ use Symfony\Component\Validator\ConstraintViolationListInterface;
 class ValidationException extends \Exception
 {
 
+    /**
+     * @return \Infrastructure\Validation\ValidationException
+     */
     public static function fromConstraintViolations(ConstraintViolationListInterface $validationErrors)
     {
         $messages = array();
         foreach ($validationErrors as $error) {
-            $messages[] = $error.'';
+            $messages[$error->getCode()] = $error.'';
         }
 
-        return new self(join("\n", $messages));
+        return new self(serialize($messages)); //TODO how to transport the messages stack to flattenexception? see exceptioncontroller
     }
 
 }
