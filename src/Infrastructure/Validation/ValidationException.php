c03aec57924902a80486e4584b0956801589ff7a
Markus <Markus@Markus-PC>
2013-09-20 17:53:15 +0200

    validation exception

--- src/Infrastructure/Validation/ValidationException.php
+++ src/Infrastructure/Validation/ValidationException.php
@@ -18,4 +18,12 @@ class ValidationException extends \Exception
         return new self(serialize($messages)); //TODO how to transport the messages stack to flattenexception? see exceptioncontroller
     }
 
+    /**
+     * @return \Infrastructure\Validation\ValidationException
+     */
+    public static function fromField($fieldname, $message)
+    {
+        return new self(serialize(array($fieldname=>$message)));
+    }
+
 }
