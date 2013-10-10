d6aa1e3124341041f0a77a17d72d6a9b03bd2006
Markus <Markus@Markus-PC>
2013-10-10 10:34:12 +0200

    minor fix, simplereader

---
+++ src/Infrastructure/Reader/SimpleReader.php
@@ -0,0 +1,46 @@
+<?php
+namespace Infrastructure\Reader;
+
+use Infrastructure\Search\Dto\FilterInterface;
+use Infrastructure\Search\Finder;
+use Infrastructure\Search\Dto\Query;
+class SimpleReader
+{
+
+    /**
+     * @var Finder
+     */
+    protected $finder;
+
+    public function __construct(Finder $finder)
+    {
+        $this->finder = $finder;
+    }
+
+    public function get($id)
+    {
+        $queryParams = array('id' => $id);
+
+        //security constraint
+        //$this->securityAdvisor->applyClientIdToQueryArray($queryParams);
+
+        $query = new Query($queryParams);
+        $query->limit = 1;
+        $objects = $this->finder->search($query);
+
+        if (count($objects) == 0) {
+            return null;
+        }
+
+        return $objects[0];
+    }
+
+    public function readWith(FilterInterface $filter)
+    {
+        $query = $filter->toQuery();
+        $data = $this->finder->search($query);
+
+        return $data;
+    }
+
+}
