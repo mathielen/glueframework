33b158ed5953383bb76b4684a1ac56eeb137bbd6
Markus Thielen <info@logicx.de>
2015-01-28 10:10:40 +0100

    Mongodb Aggregation

---
+++ src/Infrastructure/Persistence/MongoDB/MongoDBAggregation.php
@@ -0,0 +1,64 @@
+<?php
+namespace Infrastructure\Persistence\MongoDB;
+
+
+use Doctrine\MongoDB\Connection;
+use Infrastructure\Exception\ResourceNotFoundException;
+
+class MongoDBAggregation
+{
+
+    /**
+     * @var Connection
+     */
+    protected $connection;
+
+    private $executedQueries = [];
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @return \MongoClient
+     */
+    public function getMongoClient()
+    {
+        $client = $this->connection->getMongoClient();
+        if (!$client) {
+            $this->connection->connect();
+            $client = $this->connection->getMongoClient();
+        }
+
+        return $client;
+    }
+
+    public function getExecutedQueries()
+    {
+        return $this->executedQueries;
+    }
+
+    public function query($collectionName, array $query)
+    {
+        //TODO check if DB and collection exists
+        if (!in_array($collectionName, ['Order', 'Stock'])) {
+            throw new ResourceNotFoundException('Collection', $collectionName);
+        }
+
+        $collection = $this->getMongoClient()->selectDB('reporting_portal')->selectCollection($collectionName);
+
+        $ms = microtime(true);
+        $result = $collection->aggregate($query);
+        $me = microtime(true);
+
+        $this->executedQueries[] = [
+            'collection'=>$collectionName,
+            'query'=>json_encode($query),
+            'duration'=>$me-$ms
+        ];
+
+        return $result['result'];
+    }
+
+}
