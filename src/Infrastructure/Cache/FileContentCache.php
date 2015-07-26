51b376c4389d0494300460eaf9d776fa3fcec010
Markus Thielen <info@logicx.de>
2015-07-26 18:00:00 +0200

    queryresult

---
+++ src/Infrastructure/Cache/FileContentCache.php
@@ -0,0 +1,86 @@
+<?php
+namespace Infrastructure\Cache;
+
+use Doctrine\Common\Cache\Cache;
+class FileContentCache implements Cache
+{
+
+    /**
+     * The cache directory.
+     *
+     * @var string
+     */
+    protected $directory;
+
+    /**
+     * The cache file extension.
+     *
+     * @var string|null
+     */
+    protected $extension;
+
+    /**
+     * Constructor.
+     *
+     * @param string      $directory The cache directory.
+     * @param string|null $extension The cache file extension.
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct($directory, $extension = null)
+    {
+        if ( ! is_dir($directory) && ! @mkdir($directory, 0777, true)) {
+            throw new \InvalidArgumentException(sprintf(
+                    'The directory "%s" does not exist and could not be created.',
+                    $directory
+            ));
+        }
+
+        if ( ! is_writable($directory)) {
+            throw new \InvalidArgumentException(sprintf(
+                    'The directory "%s" is not writable.',
+                    $directory
+            ));
+        }
+
+        $this->directory = realpath($directory);
+        $this->extension = $extension ?: $this->extension;
+    }
+
+    private function getFilename($id)
+    {
+        $filename = preg_replace('/[^A-Za-z0-9 ]/', '', $id);
+
+        return $this->directory . DIRECTORY_SEPARATOR . $filename;
+    }
+
+    public function fetch($id)
+    {
+        if (!$this->contains($id)) {
+            return null;
+        }
+
+        return file_get_contents($this->getFilename($id));
+    }
+
+    public function contains($id)
+    {
+        return is_file($this->getFilename($id));
+    }
+
+    public function save($id, $data, $lifeTime = 0)
+    {
+        return file_put_contents($this->getFilename($id), $data);
+    }
+
+    public function delete($id)
+    {
+        return @unlink($this->getFilename($id));
+    }
+
+    public function getStats()
+    {
+        return null;
+    }
+
+}
---
+++ src/Infrastructure/Search/QueryResult.php
@@ -0,0 +1,37 @@
+<?php
+namespace Infrastructure\Search;
+
+use JMS\Serializer\Annotation\Type;
+use Pagerfanta\PagerfantaInterface;
+
+class QueryResult
+{
+
+    /**
+     * @Type("array")
+     */
+    protected $metadata = null;
+
+    /**
+     * @Type("array")
+     */
+    protected $data = array();
+
+    /**
+     * @return QueryResult
+     */
+    public static function fromDataset($data, $total=null)
+    {
+        return new self(
+            $data,
+            array('total' => is_null($total)?count($data):$total)
+        );
+    }
+
+    public function __construct($data, $metadata = null)
+    {
+        $this->data = $data;
+        $this->metadata = $metadata;
+    }
+
+}
