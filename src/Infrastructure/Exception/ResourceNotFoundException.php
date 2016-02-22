b5b9e4209465dbdc2a07804e33a7fd4205df78b8
Markus Thielen <info@logicx.de>
2016-02-22 13:30:23 +0100

    resourcenotfoundexception can have a valid-ids hint array

--- src/Infrastructure/Exception/ResourceNotFoundException.php
+++ src/Infrastructure/Exception/ResourceNotFoundException.php
@@ -6,9 +6,9 @@ use Infrastructure\Search\NotFoundException;
 class ResourceNotFoundException extends NotFoundException
 {
 
-    public function __construct($resourceName, $identifier)
+    public function __construct($resourceName, $identifier, array $validIdentifiers = [])
     {
-        parent::__construct(sprintf("Resource '%s' with id '%s' not found", $resourceName, $identifier));
+        parent::__construct(sprintf("Resource '%s' with id '%s' not found." . (empty($validIdentifiers) ? '' : ' Valid identifiers are: %s.'), $resourceName, $identifier, join(',', $validIdentifiers)));
     }
 
 }
