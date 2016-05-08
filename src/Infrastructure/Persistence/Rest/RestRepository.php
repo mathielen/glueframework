6eba7ca930bfbba8d0811769cd1c2dd42caecccd
Markus Thielen <info@logicx.de>
2016-05-08 17:31:21 +0200

    guzzle rest repository

---
+++ src/Infrastructure/Persistence/Rest/RestRepository.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Infrastructure\Persistence\Rest;
+
+
+use Doctrine\Common\Persistence\ObjectRepository;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use Infrastructure\Persistence\Repository;
+use JMS\Serializer\SerializerInterface;
+
+class RestRepository implements \Infrastructure\Persistence\Repository
+{
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    private $cls;
+
+    public function __construct(Client $client, SerializerInterface $serializer, $cls)
+    {
+        $this->client = $client;
+        $this->serializer = $serializer;
+        $this->cls = $cls;
+    }
+
+    /**
+     * returns specific connection object for this kind of repository
+     */
+    public function getConnection()
+    {
+        return $this->client;
+    }
+
+    /**
+     * persists given $object in repository
+     */
+    public function save($object)
+    {
+        // TODO: Implement save() method.
+    }
+
+    /**
+     * fetches an object identified by given $id
+     */
+    public function get($id)
+    {
+        try {
+            $response = $this->client->get($id);
+        } catch (ClientException $e) {
+            if ($e->getResponse()->getStatusCode() === 404) {
+                return null;
+            }
+
+            throw $e;
+        }
+
+        $responseJson = (string) $response->getBody();
+        return $this->serializer->deserialize($responseJson, $this->cls, 'json');
+    }
+
+    /**
+     * deletes object from repository identified by given $id
+     */
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+    
+}
