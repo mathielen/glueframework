7c8d732026c6ff8bb5721ca994f891c3bb3f2872
Markus Thielen <info@logicx.de>
2016-07-19 12:34:46 +0200

    common recursive functions

--- src/Infrastructure/Utils.php
+++ src/Infrastructure/Utils.php
@@ -172,4 +172,26 @@ class Utils
             $ranges[] = $firstItem.'-'.$lastItem;
         }
     }
+
+    public static function in_array_r($needle, $haystack, $strict = false)
+    {
+        foreach ($haystack as $item) {
+            if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && self::in_array_r($needle, $item, $strict))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static function array_unique_r(array $array)
+    {
+        foreach ($array as &$arrayCandidate) {
+            if (is_array($arrayCandidate)) {
+                $arrayCandidate = self::array_unique_r($arrayCandidate);
+            }
+        }
+
+        return array_unique($array, SORT_REGULAR);
+    }
 }
